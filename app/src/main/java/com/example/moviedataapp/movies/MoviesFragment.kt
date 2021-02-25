0d7cd7865b454dae6451a85d1cfaaaed0bf7af9d
i-vishi <rr.vishalgaur@gmail.com>
2021-02-25 17:00:05 +0530

    final changes and fixed navigation

--- app/src/main/java/com/example/moviedataapp/movies/MoviesFragment.kt
+++ app/src/main/java/com/example/moviedataapp/movies/MoviesFragment.kt
@@ -5,81 +5,91 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.moviedataapp.R
 import com.example.moviedataapp.databinding.FragmentMoviesBinding
-import kotlin.concurrent.fixedRateTimer
+
 
 class MoviesFragment : Fragment() {
 
 
-    class MovieViewModelFactory(
-        private val application: Application,
-        private val apiString: String
+	class MovieViewModelFactory(
+            private val application: Application,
+            private val apiString: String
     ) : ViewModelProvider.Factory {
-        @Suppress("UNCHECKED_CAST")
-        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-            if (modelClass.isAssignableFrom(MoviesViewModel::class.java)) {
-                return MoviesViewModel(application, apiString) as T
-            }
-            throw IllegalArgumentException("Unknown ViewModel class")
-        }
-    }
+		@Suppress("UNCHECKED_CAST")
+		override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+			if (modelClass.isAssignableFrom(MoviesViewModel::class.java)) {
+				return MoviesViewModel(application, apiString) as T
+			}
+			throw IllegalArgumentException("Unknown ViewModel class")
+		}
+	}
 
-    private lateinit var apiGetString: String
+	private lateinit var apiGetString: String
 
-    private val viewModel: MoviesViewModel by viewModels {
-        MovieViewModelFactory(requireNotNull(activity).application, apiGetString)
-    }
+	private val viewModel: MoviesViewModel by viewModels {
+		MovieViewModelFactory(requireNotNull(activity).application, apiGetString)
+	}
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+	override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View {
 
-        val binding = FragmentMoviesBinding.inflate(inflater)
+		val binding = FragmentMoviesBinding.inflate(inflater)
 
-        val arguments = MoviesFragmentArgs.fromBundle(requireArguments())
+		val arguments = MoviesFragmentArgs.fromBundle(requireArguments())
 
-        apiGetString = arguments.selectedTopicAPI
+		apiGetString = arguments.selectedTopicAPI
 
-        binding.lifecycleOwner = this
-        binding.viewModel = viewModel
+		binding.lifecycleOwner = this
+		binding.viewModel = viewModel
 
-        val activity = activity as AppCompatActivity
-        activity.supportActionBar?.title = getScreenTitle()
+		val activity = activity as AppCompatActivity
+		activity.supportActionBar?.title = getScreenTitle()
 
 
-        binding.moviesPhotoGrid.adapter = PhotoGridAdapter(PhotoGridAdapter.OnClickListener {
+		binding.moviesPhotoGrid.adapter = PhotoGridAdapter(PhotoGridAdapter.OnClickListener {
             viewModel.displayMovieDetails(it.id)
         })
 
-        viewModel.navigateToSelectedMovie.observe(viewLifecycleOwner, {
+		viewModel.navigateToSelectedMovie.observe(viewLifecycleOwner, {
             if (null != it) {
                 this.findNavController()
-                    .navigate(MoviesFragmentDirections.actionMoviesFragmentToMovieDetailFragment(it))
+                        .navigate(MoviesFragmentDirections.actionMoviesFragmentToMovieDetailFragment(it))
                 viewModel.displayMovieDetailsComplete()
             }
         })
 
-        return binding.root
-    }
+		return binding.root
+	}
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		val callback: OnBackPressedCallback = object : OnBackPressedCallback(true /* enabled by default */) {
+			override fun handleOnBackPressed() {
+				this@MoviesFragment.findNavController().navigate(MoviesFragmentDirections.actionMoviesFragmentToHomeFragment())
+			}
+		}
+		requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+
+	}
 
 
-
-    private fun getScreenTitle(): String {
-        return when(apiGetString) {
+	private fun getScreenTitle(): String {
+		return when (apiGetString) {
             context?.getString(R.string.apiTrending) -> requireContext().getString(R.string.trending)
             context?.getString(R.string.apiComingSoon) -> requireContext().getString(R.string.comingSoon)
             context?.getString(R.string.apiTopRated) -> requireContext().getString(R.string.topRated)
-            else -> requireContext().getString(R.string.app_name)
-        }
-    }
+			else -> requireContext().getString(R.string.app_name)
+		}
+	}
 }
